fe95495ebc6551c60cac615a857f02fa060ae32d
michalpytka <m.m.m.pytka@gmail.com>
2025-07-09 21:25:58 +0200

    Initiated svelte project; added dependencies

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Scene from './Scene.svelte'
+    export const prerender = true;
+    
+</script>
+
+
+<div class="canvas-container">
+<Scene/>
+</div>
+
+<style>
+    /* Set canvas to cover the whole screen */
+    .canvas-container {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100vh; /* Viewport height */
+      z-index: -1; /* Ensure canvas is behind other content */
+    }
+  
+</style>
