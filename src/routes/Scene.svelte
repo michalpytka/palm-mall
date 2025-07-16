d7c7cde22f51b51d749f34aaf68a01821ee9aa59
michalpytka <m.m.m.pytka@gmail.com>
2025-07-16 22:00:31 +0200

    Added new export of scene model; added placeholder background; added mouse controls

--- src/routes/Scene.svelte
+++ src/routes/Scene.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
   import { T, useTask } from '@threlte/core'
-  import { interactivity, useGltf } from '@threlte/extras'
-  import { Spring } from 'svelte/motion'
+  import { interactivity, OrbitControls, useGltf, Environment } from '@threlte/extras'
   
   interactivity();
-  const scale = new Spring(1);
   let rotation = $state(0);
   
   useTask((delta) => {
@@ -16,35 +14,26 @@
 
 <T.PerspectiveCamera
   makeDefault
-  position={[20, 50, 30]}
+  position={[20, 70, 30]}
   oncreate={(ref) => {
-    ref.lookAt(0, -1, -5)
+    ref.lookAt(0, 0, 0)
   }}
 />
 
 {#if $mall}
-  <T.Mesh
-    position={[20, 0, 0]}
-    rotation.y={0.2}
-    rotation.x={1.4}
-    rotation.z={-1.4}
-    geometry={$mall.nodes.Stairs.geometry}
-    scale={scale.current}
-    onpointerenter={() => {
-      scale.target = 1.5
-    }}
-    onpointerleave={() => {
-      scale.target = 1
-    }}
-    castShadow
-  >
-    <T.MeshStandardMaterial 
-      color="green"
-    />
-  </T.Mesh>
+  <T is={$mall.scene}/>
 {/if}
 
 <T.DirectionalLight 
   position={[0, 10, 10]} 
   castShadow
+/>
+
+<T.PerspectiveCamera makeDefault position={[0, 3, 150]} fov={65}>
+  <OrbitControls />
+</T.PerspectiveCamera>
+
+<Environment
+  url="wallpaper.png"
+  isBackground={true}
 />
